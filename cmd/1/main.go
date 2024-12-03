487e9ffb20efff2f4654b672a0b2c3df45758ebe
C. K. Walters <c.k.walters.co@gmail.com>
2024-12-03 13:59:29 -0500

    finish day 1 star 1

--- cmd/1/main.go
+++ cmd/1/main.go
@@ -2,34 +2,27 @@ package main
 
 import (
 	"fmt"
+	"math"
 
-	"github.com/c-k-walters/advent"
 	"github.com/c-k-walters/advent/util"
 )
 
 func main() {
     fmt.Println("Day 1 Solutions")
+    star1()
+    star2()
 }
 
 func star1() {
     fmt.Println("Star 1:")
-    a, b := util.fileToLists("input-star1.txt")
-}
-
-func calcDistance(list1, list2 []int) (int, error) {
-    sort.Ints(list1)
-    sort.Ints(list2)
-
-    if len(list1) != len(list2) {
-        return -1, errors.New("lengths of the two input list did not match")
-    }
+    a, b := util.FileToList("input-star1.txt")
 
     sum := 0
-    for i := 0; i < len(list1); i++ {
-        sum += (int) (math.Abs((float64) (list1[i] - list2[i])))
+    for i := 0; i < len(a); i++ {
+        dist := math.Abs((float64) (a[i] - b[i]))
+        sum += (int) (dist)
     }
-
-    return sum, nil
+    fmt.Println(sum)
 }
 
 func star2() {
